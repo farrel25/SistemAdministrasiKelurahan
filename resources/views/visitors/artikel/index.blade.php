d0aad891de5c03556b9fe5f00d8256cee3519bca
wahyurochman19 <wahyu.roch19@gmail.com>
2020-12-19 22:47:11 +0700

    benerin view

--- resources/views/visitors/artikel/index.blade.php
+++ resources/views/visitors/artikel/index.blade.php
@@ -12,6 +12,7 @@
             <div class="col-lg-8" data-aos="fade-right" data-aos-delay="800">
                 <div class="all-blog-posts">
                     <div class="row">
+                        @forelse ($articles as $article)
                         <div class="col-lg-12">
                             <div class="blog-post">
                                 <div class="blog-thumb">
@@ -19,17 +20,31 @@
                                 </div>
                                 <div class="down-content">
                                     <a href="post-details.html">
-                                        <h4>Article 1</h4>
+                                        <h4>{{$article->title}}</h4>
                                     </a>
                                     <ul class="post-info ">
-                                        <li><a href="#">Admin</a></li>
-                                        <li><a href="#">May 31, 2020</a></li>
+                                        <li><a href="#">{{$article->user_id}}</a></li>
+                                        <li><a href="#">{{$article->created_at}}</a></li>
                                     </ul>
-                                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                        officia consequat duis enim velit mollit. Exercitation veniam consequat sunt
-                                        nostrud amet. <a href="#">readmore..</a> </p>
+                                    <p>{{Str::limit($article->body,100)}} <a href="#">readmore..</a> </p>
                                 </div>
                             </div>
+                        </div>
+                        @empty
+                            <div class="alert alert-info">There are no post.</div>
+                        @endforelse
+                        <div class="col-lg-12 mb-5">
+                            <ul class="page-numbers">
+                                <li class="active"><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                        </svg>
+                                    </a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -40,7 +55,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
